fc88b81575ad0563f3476161b8697f4aebfc71cf
MappariChan <wantuchmatvii@gmail.com>
2023-05-15 00:40:21 +0300

    Adding routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import RootLayout from "./layouts/RootLayout";
+import HomePage from "./pages/HomePage";
+import UserPage from "./pages/UserPage";
+import OperationsPage from "./pages/OperationsPage";
+import OperationDetailPage from "./pages/OperationDetailPage";
+import LoginPage from "./pages/LoginPage";
+import RegistrationPage from "./pages/RegistrationPage";
+
 const App = () => {
-  return <div></div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { path: "", element: <HomePage /> },
+        { path: "user", element: <UserPage /> },
+        { path: "operations", element: <OperationsPage /> },
+        { path: "operations/:id", element: <OperationDetailPage /> },
+      ],
+    },
+    { path: "/login", element: <LoginPage /> },
+    { path: "/registration", element: <RegistrationPage /> },
+  ]);
+
+  return <RouterProvider router={router}></RouterProvider>;
 };
 
 export default App;
---
+++ frontend/src/pages/RegistrationPage.js
@@ -0,0 +1,5 @@
+const RegistrationPage = () => {
+  return <h1>Registartion page</h1>;
+};
+
+export default RegistrationPage;
