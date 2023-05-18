f6a30512b835e1c3c94636ecec829dd7f428ef5a
Pancakeqq <andrii.bolinovskyi@gmail.com>
2023-05-18 11:23:40 +0300

    cors added

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const dotenv = require('dotenv')
 const Product = require('./models/operation')
 const operationRoute = require('./routes/operations')
+const cors = require('cors')
 dotenv.config()
 
 const app = express();
 
-
+app.use(cors())
 app.use(express.json());
 
 app.use('/api', operationRoute)
@@ -18,5 +19,5 @@ app.use('/', (req, res) =>{
 
 
 
-
+// Create your .env file after pulling!!!
 app.listen(process.env.PORT)
