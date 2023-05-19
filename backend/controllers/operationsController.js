f222d030f7fac70190e8db0afd82960079b7933e
Pancakeqq <andrii.bolinovskyi@gmail.com>
2023-05-19 16:31:28 +0300

    operaionControllerFix

--- backend/controllers/operationsController.js
+++ backend/controllers/operationsController.js
@@ -2,33 +2,57 @@ const models = require('../models/db/Models')
 const Sequelize = require('sequelize');
 
 exports.getOneOperation = async (req, res, next) => {
-  const result = await models.userinfo.findAll();
+    const id = req.params.id
+    const email = req.user.email;
+    const opearations = await models.userinfo.findOne({
+      attributes: ['email', 'id'],
+      include: [
+        {
+          model: models.single_operation,
+          as: 'single_operations',
+          attributes: ['description', 'title', 'date', 'amount'],
+          include: [
+            {
+              model: models.goal,
+              as: 'goal',
+              attributes: ['name']
+            }
+          ]
+        },
+      ],
+      where: { email: email },
+      //where: {id : id}
+    });
+  
+    if (!opearations) res.status(141).json({ error: "sho za precoly" });
+  
+    return res.json({ result: opearations });
 };
 
 exports.getAllOperation = async (req, res, next) => {
-  const email = req.user.email;
-  const opearations = await models.userinfo.findOne({
-    attributes: ['email', 'id'],
-    include: [
-      {
-        model: models.single_operation,
-        as: 'single_operations',
-        attributes: ['description', 'title', 'date', 'amount'],
-        include: [
-          {
-            model: models.goal,
-            as: 'goal',
-            attributes: ['name']
-          }
-        ]
-      },
-    ],
-    where: { email: email },
-  });
-
-  if (!opearations) res.status(141).json({ error: "sho za precoly" });
-
-  return res.json({ result: opearations });
+    const email = req.user.email;
+    const opearations = await models.userinfo.findOne({
+      attributes: ['email', 'id'],
+      include: [
+        {
+          model: models.single_operation,
+          as: 'single_operations',
+          attributes: ['description', 'title', 'date', 'amount'],
+          include: [
+            {
+              model: models.goal,
+              as: 'goal',
+              attributes: ['name']
+            }
+          ]
+        },
+      ],
+      where: { email: email },
+    });
+  
+    if (!opearations) res.status(141).json({ error: "sho za precoly" });
+  
+    return res.json({ result: opearations.single_operations });
 };
 
 exports.postAddOperation = async (req, res, next) => {
--- backend/routes/operations.js
+++ backend/routes/operations.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 // router.get('/operations/:id', operationsController.getOneOperation)
 
+
+router.get('/operations/:id', operationsController.getOneOperation);
+
 router.get('/operations/', operationsController.getAllOperation);
 
 router.post('/operations/', operationsController.postAddOperation)
