53280ca974851441dcbcbb3c07da80dd0e3c3119
Pancakeqq <andrii.bolinovskyi@gmail.com>
2023-05-19 22:11:38 +0300

    asd

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './dist')));
 
-app.use("/api", operationRoute);
+app.use("/api",authMiddleware, operationRoute);
 
 app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, './dist/index.html'));
