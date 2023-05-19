7193b4cc0bf21b9591ad102e2bb798fadfae1ec8
OleksandrYu <yushchakop0188052@gmail.com>
2023-05-19 22:55:33 +0300

    fix: small fix

--- models/db/Models.js
+++ models/db/Models.js
@@ -6,20 +6,10 @@ dotenv.config();
 const DATABASE_URL = process.env.DATABASE_URL;
 
 const getModels = async () => {
-
   const sequelize = new Sequelize(DATABASE_URL);
-  await sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
-
   // const sequelize = new Sequelize(`postgres://${CONNECTION_USER}:${CONNECTION_PWD}@${CONNECTION_HOST}:5432/${CONNECTION_DB}`);
-
   const models = initModels(sequelize);
   return models;
 }
 
-module.exports = await getModels();
+module.exports = getModels();
