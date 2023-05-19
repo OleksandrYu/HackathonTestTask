8a49c494b5af2a4d5a39d599668f706f42204194
MappariChan <wantuchmatvii@gmail.com>
2023-05-19 13:14:31 +0300

    description fix

--- frontend/src/ui/TextArea.js
+++ frontend/src/ui/TextArea.js
@@ -2,14 +2,15 @@ import styles from "./TextArea.module.css";
 const TextArea = (props) => {
   return (
     <div className={styles.container}>
-      <label
-        htmlFor="desc"
-        className={styles.placeholder}
-        onChange={props.onChange}
-      >
+      <label htmlFor="desc" className={styles.placeholder}>
         {props.placeholder}
       </label>
-      <textarea id="desc" className={styles.text_area} required />
+      <textarea
+        id="desc"
+        className={styles.text_area}
+        required
+        onChange={props.onChange}
+      />
     </div>
   );
 };
