c9441aeb860fa9249f2b3db27b1c22f3cf218f9c
MappariChan <wantuchmatvii@gmail.com>
2023-05-19 21:42:30 +0300

    fixing registration form

--- frontend/src/components/RegistrationForm/RegistrationForm.js
+++ frontend/src/components/RegistrationForm/RegistrationForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { redirect, Form } from "react-router-dom";
 
 import classes from "./RegistrationForm.module.css";
 
@@ -7,33 +8,64 @@ import Button from "../../ui/Button";
 
 const RegistrationForm = () => {
   const [email, setEmail] = useState("");
-  const [nickname, setNickname] = useState("");
+  const [login, setLogin] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
-  const formSubmitHandler = async (event) => {
-    event.target.preventDefault();
+  // const formSubmitHandler = async (event) => {
+  //   event.preventDefault();
 
-    const user = {
-      email: email,
-      nickname: nickname,
-      password: password,
-    };
+  //   const user = {
+  //     email: email,
+  //     login: login,
+  //     password: password,
+  //     name: name,
+  //   };
 
-    const result = await fetch(`/api/registration`, {
-      method: "POST",
-      body: JSON.stringify(user),
-    });
-  };
+  //   console.log(user);
+
+  //   const result = await fetch(`http://localhost:3001/api/register`, {
+  //     method: "POST",
+  //     body: JSON.stringify(user),
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   })
+  //     .then((data) => data.json())
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+
+  //   localStorage.setItem("token", result.token);
+  //   return redirect("/");
+  // };
 
   return (
-    <form className={classes.form} onSubmit={formSubmitHandler}>
-      <Input placeholder="Email" onChange={(value) => setEmail(value)} />
-      <Input placeholder="Nickname" onChange={(value) => setNickname(value)} />
-      <Input placeholder="Password" onChange={(value) => setPassword(value)} />
+    <Form className={classes.form} method="post">
+      <Input
+        placeholder="Name"
+        onChange={(value) => setName(value)}
+        name="name"
+      />
+      <Input
+        placeholder="Email"
+        onChange={(value) => setEmail(value)}
+        name="email"
+      />
+      <Input
+        placeholder="login"
+        onChange={(value) => setLogin(value)}
+        name="login"
+      />
+      <Input
+        placeholder="Password"
+        onChange={(value) => setPassword(value)}
+        name="password"
+      />
       <div className={classes.actions}>
         <Button type="submit">Register</Button>
       </div>
-    </form>
+    </Form>
   );
 };
 
